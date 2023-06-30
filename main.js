b7483747a354d68279c8abde8346046931839442
AnonymouseHucker29 <brent.baylon29@gmail.com>
2023-06-30 12:40:10 +0800

    rich presence implementation and code improvements

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { config } = require('dotenv');
-const { Client, GatewayIntentBits, Presence } = require('discord.js');
+const { ActivityType, Client, GatewayIntentBits, Presence } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
@@ -9,16 +9,28 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
     client.user.setPresence({
-        activities: [{ name: 'with your heart </3' }],
+        activities: [{ name: 'with your heart </3', type: ActivityType.Playing }],
         status: 'idle',
     });
 
+    /** Use only if you want to change the bot's username
+     * 
+     *  client.user.setUsername('Elesis Sieghart')
+     *      .then(user => console.log(`My new username is ${user.username}`))
+     *      .catch(console.error);
+    **/
 });
 
 client.login(process.env.DISCORD_CLIENT_TOKEN);
 
 client.on('messageCreate', message => {
-    if (message.content === 'Hello') {
-        message.reply(`Hello, ${message.author}`);
+
+    // variable that prevents the bot from looping itself
+    const bot = message.author.bot;
+
+    if (message.content.toLowerCase().includes('hello') && !bot) {
+        message.reply(`Hello, ${message.author}!`);
+    } else if (message.content.toLowerCase().includes('yawa') && !bot) {
+        message.reply(`Oops, ayaw sigeg pamalikas ${message.author}!`);
     }
 });
